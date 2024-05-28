470ae606897a16747453e6af1c2eca636e4de04a
root <alpha@ubuntu.myguest.virtualbox.org>
2024-05-28 21:41:37 +0530

    Add app.js, Dockerfile, main.tf, and package.json

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "us-west-2" # Specify your desired region
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "hello-world-cluster"
+}
+
+resource "aws_ecs_task_definition" "hello_world" {
+  family                   = "hello-world-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "hello-world"
+      image     = "itachi1212/hello-world-nodejs:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "main" {
+  name            = "hello-world-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.hello_world.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = ["subnet-xxxxxxx"] # Update with your subnet ID
+    security_groups = ["sg-xxxxxxx"]      # Update with your security group ID
+  }
+}
